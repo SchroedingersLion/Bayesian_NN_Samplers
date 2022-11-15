94dcc22550436ea07571f3d5b86da7f489a44adb
Rene Lohmann <R.Lohmann@ed.ac.uk>
2022-11-15 22:00:44 +0100

    Update BNN_samplers.py
    - Extended introduction
    - Added AMAGOLD

--- BNN_samplers.py
+++ BNN_samplers.py
@@ -12,13 +12,17 @@ SGHMC_scaled = Scaled version of SGHMC to compare it to SGD with momentum, see a
 OBABO - The OBABO splitting integrator for Langevin Dynamics as introduced by Bussi & Parrinello, 2007.
 HMC - Hamiltonian Monte Carlo, see eg. Neal 2012. It can also be used with stochastic gradients (not to be
       confused with SGHMC).
+AMAGOLD - Metropolized Langevin dynamics using stochastic gradients, see Zhang et al., 2020.
 
 
 train_optimizer - Function to train the model conventionally by using the passed optimizer (such as torch.optimizer.SGD).
 
-Open issue: The kinetic energies and L2 norms are computed layer-wise. Each column corresponds to one layer.
-            Currently, the number of layers is trivially computed, but assumes a fully-connected model with 
-            two parameters per layer (i.e. weight and bias). For other network types, this needs to be adjusted.
+
+Open issues: 1) The kinetic energies and L2 norms are computed layer-wise. Each column corresponds to one layer.
+             Currently, the number of layers is trivially computed, but assumes a fully-connected model with 
+             two parameters per layer (i.e. weight and bias). For other network types, this needs to be adjusted.
+             
+             2) Loss criterions are assumed to be passed with reduction mode "mean".
 """
 
 import torch
@@ -29,6 +33,7 @@ import copy
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")  ## use GPU if available
 
 #%% SGHMC
+
 
 class SGHMC(nn.Module):
     """
@@ -51,7 +56,7 @@ class SGHMC(nn.Module):
         """
         Main train/sampling routine. Returns arrays of losses and accuracies on both train and test set. 
         Also returns arrays of kinetic energies and squared-L2 norm of network parameters (each column corresponds
-        corresponds to single network layer).
+        to single network layer).
         """        
         
         loss_train = np.zeros(self.epochs+1)                 
@@ -68,8 +73,8 @@ class SGHMC(nn.Module):
         datasize = len(self.train_loader.dataset)
         
         squeeze = True if type(self.criterion) == torch.nn.modules.loss.BCELoss else False   # squeeze network output
-                                                                                             # for BCELoss (not required
-                                                                                             # for NLLLoss)
+                                                                                              # for BCELoss (not required
+                                                                                              # for NLLLoss)
         
         start_time = time.time()
         print("Starting SGHMC sampling...")
@@ -295,7 +300,7 @@ class OBABO(nn.Module):
         self.fill_gradients(data, target, squeeze, datasize)
                 
         for p in self.model.parameters():                       # create momentum buffers
-            p.buf = torch.zeros(p.size()).to(device)
+            p.buf = torch.randn(p.size()).to(device)
 
         for (idx,p) in enumerate(self.model.parameters()):                                      # initial kin. energies
             kin_energy[0,idx//2] += (p.buf**2).sum() ; L2weight[0,idx//2] = (p.data**2).sum()   # and L2 norms 
@@ -441,7 +446,7 @@ class HMC(nn.Module):
             K0 = 0
             for p in list(self.model.parameters()):                 # resample momentum and 
                 p.buf = torch.randn(p.size()).to(device)            # store initial kin. energy
-                K0 += (p.buf**2).sum()
+                K0 += (p.buf**2).sum().item()
             K0 *= 0.5
                 
             ## INNER LOOP / L leapfrog steps     
@@ -461,6 +466,8 @@ class HMC(nn.Module):
             
             ## Metropolis            
             (accept, U1) = self.MH_decision(U0, K0, squeeze)
+            accept = True
+            print("always accept!!")
             
             if( accept ):          # accept sample
                
@@ -520,8 +527,8 @@ class HMC(nn.Module):
         for p in list(self.model.parameters()):     # get kin. energies
             K1 += (p.buf**2).sum() 
         K1 = 0.5*K1.item()
-        
-        MH = torch.exp( -1 * (U1 - U0 + K1-K0) )    # evaluate MH-term
+
+        MH = np.exp( -1 * (U1 - U0 + K1-K0) )    # evaluate MH-term
         
         accept = torch.rand(1) < min(1., MH)    # true for accept
         
@@ -554,6 +561,203 @@ class HMC(nn.Module):
         U += 0.5 * self.weight_decay * L2.item()
         
         return U
+
+#%% AMAGOLD
+
+class AMAGOLD(nn.Module):
+    
+    def __init__(self, model, train_loader, test_loader, criterion, lr, gamma, weight_decay, epochs, L, resample):
+        super(AMAGOLD, self).__init__()
+        self.model = model
+        self.train_loader = train_loader
+        self.test_loader = test_loader
+        self.criterion = criterion
+        self.lr = lr
+        self.gamma = gamma
+        self.weight_decay = weight_decay
+        self.epochs = epochs
+        self.L = L
+        self.resample = resample
+        
+        self.BBK_fac1 = 1 - self.lr * self.gamma/2      # constants used in integrator
+        self.BBK_fac2 = 1 / (1 + self.lr * self.gamma/2)
+        self.noise_fac = np.sqrt(2*self.lr*self.gamma)
+        
+    def train(self):
+        """
+        Main train/sampling routine. Returns arrays of losses and accuracies on both train and test set.
+        """
+        
+        datasize = len(self.train_loader.dataset)
+        
+        N = self.epochs / (self.L * self.train_loader.batch_size / datasize + 1)    # number of outer loop iterations,
+        N = int(np.rint( N + 0.5 ))                                                 # i.e. samples to take, for the desired
+                                                                                    # number of epochs
+        loss_train = np.zeros(N+1)                
+        accu_train = np.zeros(N+1)                 
+        loss_test = np.zeros(N+1)
+        accu_test = np.zeros(N+1)
+        
+        (loss_train[0], accu_train[0]) = self.model.evaluate(self.train_loader)     # initial losses and accuracies
+        (loss_test[0], accu_test[0]) = self.model.evaluate(self.test_loader)        
+        
+        squeeze = True if type(self.criterion) == torch.nn.modules.loss.BCELoss else False   # squeeze network output
+                                                                                              # for BCELoss (not required
+                                                                                              # for NLLLoss)
+        mom_old = []
+        for p in self.model.parameters():                   # create momentum buffers
+            p.buf = torch.randn(p.size()).to(device) 
+            mom_old += [p.buf.clone()]                      # needed to store old momenta for energy accumulator rho
+
+        param_list_curr = copy.deepcopy(list(self.model.parameters()))  # buffer for the params before the BAB steps
+        
+        U0 = self.get_energy(squeeze)  # get pot. energy
+        
+        ctr = 0  # count accepted moves
+        
+        
+        start_time = time.time()
+        print("Starting AMAGOLD sampling... \nPerform {} epochs at batch size {} and L={},"\
+              " i.e. {} outer loop iterations.".format(self.epochs, self.train_loader.batch_size, self.L, N))     
+        
+        # OUTER LOOP
+        for n in range(1,N+1):  
+            
+            
+            rho = 0.0
+            
+            if self.resample == True:
+                for p in list(self.model.parameters()):                 # resample momentum
+                    p.buf = torch.randn(p.size()).to(device)            
+                
+            ## INNER LOOP     
+            for batch_idx, (data, target) in enumerate(self.train_loader):
+                if batch_idx == self.L: break
+                
+                for p in list(self.model.parameters()):
+                    p.data.add_(p.buf, alpha=self.lr)                               # parameter update
+                    
+                data, target = data.to(device), target.to(device)                   # compute new gradients
+                self.fill_gradients(data, target, squeeze, datasize)
+                
+                self.update_momenta(mom_old, rho)                                   # update momenta (and rho)
+            
+           
+            ## METROPOLIS           
+            (accept, U1) = self.MH_decision(U0, rho, squeeze)
+            # accept = True; U1=1
+            # print("always accept!!")
+            
+            if( accept ):          # accept sample
+               
+                (loss_train[n], accu_train[n]) = self.model.evaluate(self.train_loader)  # losses/accuracies of proposed
+                (loss_test[n], accu_test[n]) = self.model.evaluate(self.test_loader)     # sample
+                
+                param_list_curr = copy.deepcopy(list(self.model.parameters()))  # update parameters with proposal
+                U0 = U1
+                ctr += 1
+                
+            else:                   # reject sample
+                
+                (loss_train[n], accu_train[n]) = (loss_train[n-1], accu_train[n-1])   # take losses/accuracies of
+                (loss_test[n], accu_test[n]) = (loss_test[n-1], accu_test[n-1])       # previous sample.
+                
+                with torch.no_grad():                                           
+                    for (idx,p) in enumerate(list(self.model.parameters())):
+                        p.copy_(param_list_curr[idx])                                 # restore model parameters
+                
+
+            
+            if n%5==0:
+                print("AMAGOLD ITERATION {} DONE!".format(n))
+        
+        end_time = time.time()
+        print("Training took {} seconds, i.e {} minutes, with {} seconds per epoch!"
+              .format(end_time-start_time, (end_time-start_time)/60, (end_time-start_time)/self.epochs))
+        print("Total acceptance rate: {}".format(ctr/N))
+            
+        return (loss_train, loss_test, accu_train, accu_test)
+        
+
+
+    def fill_gradients(self, data, target, squeeze, datasize):
+        """
+        Fills gradients of the model on batch (data, target).
+        """
+        
+        self.model.zero_grad()
+        output = self.model(data)
+        if squeeze: output=output.squeeze()
+        loss = self.criterion(output, target)*datasize
+        loss.backward()
+        
+        for p in list(self.model.parameters()):                 # weight decay / prior component
+            p.grad.data.add_(p.data, alpha=self.weight_decay)  
+        
+            
+        
+    def MH_decision(self, U0, rho, squeeze):
+        """
+        Evaluates Metropolis criterion. Returns boolean (true=accept, false=reject) and new potential energy.
+        """
+
+        U1 = self.get_energy(squeeze)  # get pot. energy
+        
+        MH = np.exp( -1 * (U1 - U0 - rho) )    # evaluate MH-term
+        
+        accept = torch.rand(1) < min(1., MH)    # true for accept
+        
+        return (accept, U1)      
+        
+            
+        
+    def get_energy(self, squeeze):
+        """
+        Computes potential energy for evaluation of Metropolis criterion.
+        """
+        
+        # first the likelihood part...
+        loss = 0
+        self.criterion.reduction = "sum"
+        with torch.no_grad():
+            for batch_idx, (data, target) in enumerate(self.train_loader):
+                data, target = data.to(device), target.to(device)
+                output = self.model(data)
+                if squeeze: output=output.squeeze()
+                loss += self.criterion(output, target)
+        U = loss.item()
+        self.criterion.reduction = "mean"
+        
+        # ... then the prior / L2 part
+        L2 = 0
+        for p in list(self.model.parameters()):
+            L2 += (p.data**2).sum()
+        
+        U += 0.5 * self.weight_decay * L2.item()
+        
+        return U
+    
+    
+    
+    def update_momenta(self, mom_old, rho):
+        """
+        Momentum update for all parameters. Also updates \rho, the energy accumulator used for MH criterion.
+        """
+        
+        for (idx,p) in enumerate(list(self.model.parameters())):               
+            
+            mom_old[idx].copy_(p.buf)                                       # store old momentum
+            
+            eps = torch.randn(p.size()).to(device)                          # momentum update
+            p.buf.mul_(self.BBK_fac1)
+            p.buf.add_(eps, alpha=self.noise_fac) 
+            p.buf.add_(p.grad.data, alpha=-self.lr)
+            p.buf.mul_(self.BBK_fac2)                     
+            
+            mom_old[idx].add_(p.buf)                                        # add both momenta
+            mom_old[idx].mul_(p.grad.data)                                  # scalar product with force
+            
+            rho += 0.5 * self.lr * mom_old[idx].sum()                       # update rho
 
 
 #%% Optimizer
